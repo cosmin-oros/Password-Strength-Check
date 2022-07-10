360c773b3155a3333772a3bc6c05b705c7e8521c
cosmin-oros <oroscosmin09@gmail.com>
2022-07-10 10:40:41 +0300

    encrypt decrypt file

---
+++ fileEncrypterDecrypter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "This is a simple file encrypter/decrypter"
+echo "Please choose what you want to do"
+
+choice="Encrypt Decrypt"
+
+select option in $choice; do
+        if [ $REPLY = 1 ];
+then
+        echo "You have selected Encryption"
+        echo "Please enter the file name"
+        read file;
+        gpg -c $file
+        echo "The file has been encrypted"
+else
+        echo "You have selected decryption"
+        echo "Please enter the file name"
+        read file2;
+        gpg -d $file2
+        echo "The file has been decrypted"
+fi
+
+done
